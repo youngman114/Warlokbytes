14442ef52e88e943b625a5ba234ef3c116d35160
youngman114 <artur.roos@mail.ru>
2020-09-20 18:14:50 +0300

    Commented different bytecodes. Removed deprecated STATE_HALT.

--- src/_defines.h
+++ src/_defines.h
@@ -8,8 +8,14 @@ typedef float f32;
 typedef float f64;
 typedef u32 ptr;
 
+/* BASIC INSTRUCTIONS*/
+// Do nothing
 #define PASS 0x00
-#define HALT 0xFF
+
+/* ARITHMETIC INSTRUCTIONS */
+// But next byte onto the stack
 #define PUSH 0x10
 
-#define STATE_HALT 0x11
+/* MISC INSTRUCTIONS */
+// End the program execution with TOS exit code
+#define HALT 0xFF
