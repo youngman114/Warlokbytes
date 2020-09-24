4211b87e2326200a5828be074ce33b2456025bda
youngman114 <artur.roos@mail.ru>
2020-09-24 21:30:53 +0300

    Byte Stack now wraps around std::vector<byte>

--- src/ByteStack.hpp
+++ src/ByteStack.hpp
@@ -8,33 +8,23 @@
 namespace Warlokbytes {
     class ByteStack {
     private:
-        std::size_t size;
-        std::size_t counter;
-        byte* data = nullptr;
+        std::vector<byte> data;
 
     public: 
         void Push(byte val) {
-            data[++counter] = val; 
+            data.push_back(val);
         }
 
         byte Pop() {
-            return data[counter--];
+            byte val = data.back();
+            data.pop_back();
+            return val;
         }
 
         byte Peek() {
-            return data[counter];
+            return data.back();
         }
 
-        ByteStack(std::size_t size) {
-            this->size = size;
-            this->counter = -1;
-            data = new byte[size];
-        }
-
-        ~ByteStack() {
-            for (int i = 0; i < size; i++) {
-                delete (byte*)((std::size_t)(data) + i);
-            }
-        }
+        ByteStack(std::size_t size) : data(size) {}
     };
 }
