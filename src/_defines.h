0ec8468e8c5687c1f78c19f82005098a0ea50fdb
youngman114 <artur.roos@mail.ru>
2020-09-24 22:48:24 +0300

    Removed all unused defines. Moved instructions into _instructions.hpp

--- src/_defines.h
+++ src/_defines.h
@@ -1,37 +1,5 @@
 #pragma once
 
 typedef unsigned char byte;
-
 typedef unsigned int u32;
 typedef unsigned long u64;
-typedef signed int i32;
-typedef signed long i64;
-typedef float f32;
-typedef float f64;
-typedef u32 ptr;
-
-/* BASIC INSTRUCTIONS*/
-// Do nothing
-#define PASS 0x00
-// Print NULL terminated string from stack
-#define PRINT 0x01
-
-/* STACK INSTRUCTIONS */
-// But next byte onto the stack
-#define PUSHB 0x10
-// Push NULL terminated string onto the stack
-#define PUSHN 0x11
-
-/* ARITHMETIC INSTRUCTIONS */
-// Add two numbers at stack and push result back
-#define ADD 0x20
-// Subtract two numbers at stack and push result back
-#define SUB 0x21
-// Multiply two numbers at the stack
-#define MUL 0x22
-// Divide two numbers
-#define DIV 0x23
-
-/* MISC INSTRUCTIONS */
-// End the program execution with TOS exit code
-#define HALT 0xFF
--- src/_instructions.hpp
+++ src/_instructions.hpp
@@ -3,6 +3,35 @@
 #include "src/Environment.hpp"
 #include "_defines.h"   
 
+
+/* BASIC INSTRUCTIONS*/
+// Do nothing
+#define PASS 0x00
+// Print NULL terminated string from stack
+#define PRINT 0x01
+
+/* STACK INSTRUCTIONS */
+// But next byte onto the stack
+#define PUSHB 0x10
+// Push NULL terminated string onto the stack
+#define PUSHN 0x11
+
+/* ARITHMETIC INSTRUCTIONS */
+// Add two numbers at stack and push result back
+#define ADD 0x20
+// Subtract two numbers at stack and push result back
+#define SUB 0x21
+// Multiply two numbers at the stack
+#define MUL 0x22
+// Divide two numbers
+#define DIV 0x23
+
+/* MISC INSTRUCTIONS */
+// End the program execution with TOS exit code
+#define HALT 0xFF
+
+
+
 #define NEXT env->GetCode()[++(env->GetCounter())]
 
 #define Push(x) env->currStack.push_back(x)
